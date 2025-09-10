443e8b66ab9e98bc2341e9f3974c9562292dae08
Hycity <1006853851@qq.com>
2025-09-10 21:33:11 +0800

    整理代码

--- utils/system_init.go
+++ utils/system_init.go
@@ -14,63 +14,64 @@ import (
 	"gorm.io/gorm/logger"
 )
 
+// 数据库全局共享
 var (
 	DB  *gorm.DB
 	Red *redis.Client
 )
 
+// 初始化配置文件
 func InitConfig() {
-
 	viper.SetConfigName("app")
 	viper.AddConfigPath("config")
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println("配置文件 app:", viper.Get("app"))
-	fmt.Println("配置文件 mysql:", viper.Get("mysql"))
 }
 
 func InitMysql() {
-	newlog := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			SlowThreshold: time.Second,
-			LogLevel:      logger.Info,
-			Colorful:      true,
-		},
+	// 自定义logger用于gorm日志记录
+	log_writer := log.New(os.Stdout, "\r\n", log.LstdFlags)
+	log_config := logger.Config{
+		SlowThreshold: time.Second,
+		LogLevel:      logger.Info,
+		Colorful:      true,
+	}
+
+	newlog := logger.New(log_writer, log_config)
+
+	var (
+		username = viper.GetString("mysql.username")
+		password = viper.GetString("mysql.password")
+		host     = viper.GetString("mysql.host")
+		port     = viper.GetInt("mysql.port")
+		dbname   = viper.GetString("mysql.dbname")
+		dsn      = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbname)
 	)
 
-	username := viper.GetString("mysql.username")
-	password := viper.GetString("mysql.password")
-	host := viper.GetString("mysql.host")
-	port := viper.GetInt("mysql.port")
-	dbname := viper.GetString("mysql.dbname")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbname)
-
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: newlog})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: newlog})
+	DB = db
 
 	if err != nil {
 		fmt.Println("Failed to connect to database:", err)
 		return
 	}
-
-	fmt.Println("Connected to MySQL database!")
 }
 
 func InitRedis() {
-	Red = redis.NewClient(&redis.Options{
+	redis_opt := redis.Options{
 		Addr:     viper.GetString("redis.Addr"),
 		Password: viper.GetString("redis.Password"),
 		DB:       2,
 		PoolSize: 30,
-	})
+	}
+	Red = redis.NewClient(&redis_opt)
 }
 
-const (
-	PublishKey = "websocket"
-)
+const PublishKey = "websocket"
 
 // Publish 发布消息到Redis
 func Publish(ctx context.Context, channel string, msg string) error {
@@ -82,7 +83,7 @@ func Publish(ctx context.Context, channel string, msg string) error {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	return err
 }
 
@@ -90,11 +91,13 @@ func Publish(ctx context.Context, channel string, msg string) error {
 func Subscribe(ctx context.Context, channel string) (string, error) {
 	sub := Red.Subscribe(ctx, channel)
 	fmt.Println("Subscribe 。。。。", ctx)
+
 	msg, err := sub.ReceiveMessage(ctx)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
+
 	fmt.Println("Subscribe 。。。。", msg.Payload)
 	return msg.Payload, err
 }
